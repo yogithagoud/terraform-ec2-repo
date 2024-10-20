268936a4ea6167738f5478c5d53e04078a03a3ab
rahulkalithkar <66064921+rahulkalithkar@users.noreply.github.com>
2024-10-20 16:10:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "us-east-2"
+  default     = "ap-southeast-1"
 }
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "terra"
+  default     =  "Singapore"
 }
 
 variable "instance_type" {
@@ -15,15 +15,15 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "my-jenkins-security-group"
+  default     = "launch-wizard-3"
 }
 
 variable "tag_name" {
   description = "Tag Name of for Ec2 instance"
-  default     = "my-terraform-instace"
+  default     = "my-web-app"
 }
 
 variable "ami_id" {
   description = "AMI for Amazon Ami2 Ec2 instance"
-  default     = "ami-002068ed284fb165b"
+  default     = "ami-04b6019d38ea93034"
 }
